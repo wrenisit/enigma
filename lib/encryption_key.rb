426e5e7d8d413fea48946d7ae3220ffff7a46921
Wren Steitle <lauren.steitle@gmail.com>
2019-11-01 11:39:19 -0600

    Complete random number generator test

--- lib/encryption_key.rb
+++ lib/encryption_key.rb
@@ -2,4 +2,12 @@ class EncryptionKey
 
   def initialize
   end
+
+  def random_number_generator
+    numbers_generated = []
+    5.times do |number|
+      numbers_generated << (rand(0...9))
+    end
+    numbers_generated
+  end
 end
