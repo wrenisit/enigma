88c82c630c855e6932ca9aefea802da4d4156a0e
Wren Steitle <lauren.steitle@gmail.com>
2019-11-01 11:06:30 -0600

    create test encryption key exists

--- test/encryption_key_test.rb
+++ test/encryption_key_test.rb
@@ -4,5 +4,8 @@ require './lib/encryption_key'
 require 'pry'
 
 class EncryptionKeyTest < MiniTest::Test
-
+  def test_it_exists
+    encryption_key = EncryptionKey.new
+    assert_instance_of EncryptionKey, encryption_key
+  end
 end
