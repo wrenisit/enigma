c0ad4b815157c42486864181e52a7d4fa33998c3
Wren Steitle <lauren.steitle@gmail.com>
2019-11-01 11:45:45 -0600

    Create test that it takes a date

--- test/encryption_key_test.rb
+++ test/encryption_key_test.rb
@@ -5,16 +5,21 @@ require 'pry'
 
 class EncryptionKeyTest < MiniTest::Test
   def test_it_exists
-    encryption_key = EncryptionKey.new
+    encryption_key = EncryptionKey.new("011119")
     assert_instance_of EncryptionKey, encryption_key
   end
 
   def test_it_makes_random_number
-    encryption_key = EncryptionKey.new
+    encryption_key = EncryptionKey.new("011119")
     assert_equal 5, encryption_key.random_number_generator.length
     random_set_a = encryption_key.random_number_generator
     random_set_b = encryption_key.random_number_generator
     refute random_set_a == random_set_b
     assert_equal random_set_a.length, random_set_b.length
+  end
+
+  def test_it_takes_a_date
+    encryption_key = EncryptionKey.new("011119")
+    assert_equal "011119", encryption_key.date
   end
 end
