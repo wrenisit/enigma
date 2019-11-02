2a4b33a80f39ebbe6c27c69c95a421fa08e253bf
Wren Steitle <lauren.steitle@gmail.com>
2019-11-01 20:45:00 -0600

    Complete initialization testing

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,4 +1,5 @@
 class Encrypt
+  attr_reader :encryption_key, :message
   def initialize(encryption_key, message)
     @encryption_key = encryption_key
     @message = message
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -7,12 +7,17 @@ require 'mocha/minitest'
 
 class EncryptTest < MiniTest::Test
   def setup
-    message = "hello world"
-    key = EncryptionKey.new("040211")
-    @encrypt = Encrypt.new(key, message)
+    @message = "hello world"
+    @key = EncryptionKey.new("040211")
+    @encrypt = Encrypt.new(@key, @message)
   end
   def test_it_exists
     assert_instance_of Encrypt, @encrypt
+  end
+
+  def test_initialization_data
+    assert_equal @key, @encrypt.encryption_key
+    assert_equal @message, @encrypt.message
   end
 
 end
