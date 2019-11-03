71e92ed4c5cf67a8ac6e17528c6c34d5f9c62319
Wren Steitle <lauren.steitle@gmail.com>
2019-11-02 21:20:07 -0600

    Complete rotate method

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,15 +1,18 @@
 class Encrypt
-  attr_reader :encryption_key, :message
+  attr_reader :encryption_key, :message, :alphabet
   def initialize(encryption_key, message)
     @encryption_key = encryption_key
     @message = message
   end
 
   def alphabet_array
-    alphabet = []
+    @alphabet = []
     ("a".."z").each { |letter|  alphabet << letter }
-    alphabet << " "
+    @alphabet << " "
   end
 
-
+  def rotate(number)
+    alphabet_array
+    @alphabet.rotate(number).first
+  end
 end
