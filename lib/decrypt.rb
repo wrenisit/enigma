c5120a24f8099b3963b0044a4ea8a985f1e1ce18
Wren Steitle <lauren.steitle@gmail.com>
2019-11-04 15:18:54 -0700

    Require module in decrypt method

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,4 +1,6 @@
+require_relative 'encryption_help_module'
 class Decrypt
+  include EncryptionHelpModule
   attr_reader :decryption_key, :message
   def initialize(decryption_key, message)
     @decryption_key = decryption_key
