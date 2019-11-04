d11382100a19ac0507141ac3fd6413675297b669
Wren Steitle <lauren.steitle@gmail.com>
2019-11-04 15:34:56 -0700

    Create test it decrypts message

---
+++ test/decrypt_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/decrypt'
+require './lib/encryption_key'
+require 'pry'
+require 'mocha/minitest'
+
+class DecryptTest < MiniTest::Test
+  def setup
+    @message = "vbydbxigeiq"
+    @key = EncryptionKey.new("011119")
+    @key.stubs(:random_number_generator).returns([1,2,3,4,5])
+    @decrypt = Decrypt.new(@key.offset_maker, @message)
+  end
+
+  def test_it_exists
+    assert_instance_of Decrypt, @decrypt
+  end
+
+  def test_it_initializes
+    assert_equal @message, @decrypt.message
+    expected = {a:14 , b:24 , c:40 , d:46}
+    assert_equal expected, @decrypt.decryption_key
+  end
+
+  def test_it_decrypts_messages
+    assert_equal "hello world", @decrypt.secret_message(@message)
+  end
+end
