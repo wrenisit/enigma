3bae3cc2d2966bd5794c0b025835665063d1022e
Wren Steitle <lauren.steitle@gmail.com>
2019-11-04 14:54:34 -0700

    Create Decrypt class test

---
+++ test/decrypt_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/encrypt'
+require './lib/encryption_key'
+require 'pry'
+require 'mocha/minitest'
+
+class DecryptTest < MiniTest::Test
+
+end
