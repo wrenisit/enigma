babc0e4a84819561eb6931d2ea71fddd96a28ec9
Wren Steitle <lauren.steitle@gmail.com>
2019-11-04 14:59:35 -0700

    Create setup for testing decrypt method

--- test/decrypt_test.rb
+++ test/decrypt_test.rb
@@ -1,10 +1,15 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/encrypt'
+require './lib/decrypt'
 require './lib/encryption_key'
 require 'pry'
 require 'mocha/minitest'
 
 class DecryptTest < MiniTest::Test
-
+  def setup
+    @message =
+    @key = EncryptionKey.new("011119")
+    @key.stubs(:random_number_generator).returns([1,2,3,4,5])
+    @decrypt = Decrypt.new(@key.offset_maker, @message)
+  end
 end
