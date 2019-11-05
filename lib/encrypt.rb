a61c857eb3c991ef855d89b4282dafddc683af00
Wren Steitle <lauren.steitle@gmail.com>
2019-11-05 15:59:32 -0700

    Complete encryption writing of object

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -3,7 +3,8 @@ require_relative 'enigma'
 message_file_name = ARGV[0]
 encrypted_file_name = ARGV[1]
 message = File.open("./lib/#{message_file_name}", "r").read
-encrypted_message = Enigma.new(message)
+encrypted_message_object = Enigma.new(message)
+encrypted_message = encrypted_message_object.encrypt
 encrypted_file = File.open("./lib/#{encrypted_file_name}", "w")
 encrypted_file.write(encrypted_message)
 encrypted_file.close
