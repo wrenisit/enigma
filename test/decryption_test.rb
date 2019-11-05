b9d34f890834820b17848b83bf7ae1505d067f7d
Wren Steitle <lauren.steitle@gmail.com>
2019-11-04 19:30:51 -0700

    Update Decrypt class to Decryption class to conform with interaction pattern

--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -1,29 +1,29 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/decrypt'
+require './lib/decryption'
 require './lib/encryption_key'
 require 'pry'
 require 'mocha/minitest'
 
-class DecryptTest < MiniTest::Test
+class DecryptionTest < MiniTest::Test
   def setup
     @message = "vbydbxigeiq"
     @key = EncryptionKey.new("011119")
     @key.stubs(:random_number_generator).returns([1,2,3,4,5])
-    @decrypt = Decrypt.new(@key.offset_maker, @message)
+    @decryption = Decryption.new(@key.offset_maker, @message)
   end
 
   def test_it_exists
-    assert_instance_of Decrypt, @decrypt
+    assert_instance_of Decryption, @decryption
   end
 
   def test_it_initializes
-    assert_equal @message, @decrypt.message
+    assert_equal @message, @decryption.message
     expected = {a:14 , b:24 , c:40 , d:46}
-    assert_equal expected, @decrypt.decryption_key
+    assert_equal expected, @decryption.decryption_key
   end
 
   def test_it_decrypts_messages
-    assert_equal "hello world", @decrypt.known_message(@message)
+    assert_equal "hello world", @decryption.known_message(@message)
   end
 end
