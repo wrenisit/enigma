ac2fb37b97e837985d78f979b4f8c64c28faf67d
Wren Steitle <lauren.steitle@gmail.com>
2019-11-04 19:51:09 -0700

    Create initialize data test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,4 +12,11 @@ class EnigmaTest < MiniTest::Test
     enigma = Enigma.new
     assert_instance_of Enigma, enigma
   end
+
+  def test_initialize_data
+    enigma = Enigma.new("hello", "12345", "041119")
+    assert_equal "hello", enigma.message
+    assert_equal "12345", enigma.key
+    assert_equal "041119", enigma.date
+  end
 end
